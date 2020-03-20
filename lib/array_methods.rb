0bdb823713761744a3c989ce0eb26a8acd66ec9e
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-20 16:01:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,12 +7,12 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  array.sort
+  array.sort!
   return array.last
 end
 
 def find_min_value(array)
   # Add your solution here
-  array.sort
+  array.sort!
   return array.first
 end
