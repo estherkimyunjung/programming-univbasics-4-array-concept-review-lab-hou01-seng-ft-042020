dfbb1e0ec1cfb8e0855d1f577d44bc16316fc1c1
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-20 15:52:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,20 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  if array.include?(value_to_find) do
+    return array.index(value_to_find)
+  end  
 end
 
 def find_max_value(array)
   # Add your solution here
+  new_array = array.uniq
+  new_array = array.sort
+  return new_array[0]
 end
 
 def find_min_value(array)
   # Add your solution here
+  new_array = array.uniq
+  new_array = array.reverse
+  return new_array[0]
 end
